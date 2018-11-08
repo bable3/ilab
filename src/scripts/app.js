f31f650362f2e8ff36252836f195621a072a00a8
François Faucon <francois@MacBook-Pro-de-Francois-3.local>
2018-11-08 20:20:58 +0100

    travail sur le modules

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,8 @@
 import hello from './components/hello';
 import './components/map';
 import { name } from './components/hello2';
-
+import PinchZoom from 'pinch-zoom-js'
+console.info(PinchZoom);
 
 console.info(`Hello ${hello.name}`);
 console.info(`Hello ${name()}`);
